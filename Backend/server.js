513b2cc812413ab07f8d3f7601726f02dcfe794e
adarsh946 <adarsh.shukla2724@gmail.com>
2024-12-18 22:10:09 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -20,10 +20,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/user", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
